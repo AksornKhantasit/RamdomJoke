999914aa9680e62774981cc0b9feaca21a83f7cc
Aksorn Khantasit <aksorn.khan@gmail.com>
2019-08-08 14:04:18 +0700

    Initial Commit

---
+++ RandomJoke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomJoke
+//
+//  Created by Aksorn Khantasit on 8/8/2562 BE.
+//  Copyright © 2562 Aksorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
